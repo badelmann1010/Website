8dbcb41a4bcfd6ddc27679225c69cd836218f6a5
badelmann1010 <41636250+badelmann1010@users.noreply.github.com>
2018-08-06 15:36:48 -0500

    bam

---
+++ petface.js
@@ -0,0 +1,14 @@
+ function login () {
+    var loginStuff = document.getElementById("loginInfoBox");
+    loginStuff.classList.remove("hidden");
+    document.getElementById("loginInfoBox").classList.remove= "hidden";
+
+      /*var username = document.getElementsByName("username").value;
+    var password = document.getElementsByName("password").value;
+  
+      if (!username || !password) {
+        $('.alert').show();
+      } else {
+        document.getElementById("wrongpassword").innerHTML = "Thank u very much";
+      }*/
+   }
